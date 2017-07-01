98ea60a4758f72c3b8664123ec60465761b6137c
Imagine Kawabe <imaginekawabe@gmail.com>
2017-07-01 18:40:46 +0900

    よっしゃーーーーーー

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@ Template Name: contact
 
 <?php get_header(); ?>
 
-
+hogehgoeogyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 
 
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,88 +1,231 @@
 <?php get_header(); ?>
 
-
-
-<div id="index-top" style="margin-top: 60px; text-align: center;" class="container">
-  <div class="row">
-    <div class="col-md-offset-2 col-md-8 col-sm-12">
-      <?php 
-      echo do_shortcode("[metaslider id=138]"); 
-      ?>
+  <div id="top-image">
+    <div class="container">
+      <div class="row" style="margin-top: 220px;">
+        <div class="col-md-6" style="padding-left: 80px;">
+          <h1 id="top-message" style="color: white;">人に<br>歴史あり。</h1>
+        </div>
+        <div class="col-md-6 none">
+          <p style="font-size: 20px; padding-top: 33px; color: white;">直想便は「孫 ✕ 祖父母」にフォーカスした<br>フォトグラファー派遣サービスです。<br>ただ撮るだけではなく、<br>想い、想われ、<br>想い合う時間を提供します。</p>
+        </div>
+      </div>
     </div>
   </div>
   
 </div>
 
 <div class="container">
-  <div class="row">
-    <div class="col-md-12" style="text-align: center; margin: 30px 0;">
-        <h1 id="typed-letter">
-          <a href="" class="typewrite" data-period="2000" data-type='[ "好きな言葉は？", "将来の夢は？", "一番の後悔は？", "人生の教訓は？" ]'>
-            <span class="wrap"></span>
-          </a>
-        </h1>
+  <div class="row" style="margin: 40px 0;">
+    <div class="col-md-3" style="margin: 30px 0;">
+      <h2 class="roman"><span class="yellow">C</span>hokuso<br><span class="yellow">P</span>ost</h2>
     </div>
-  </div>
-  <div class="row" style="background-color: #f6f7f9; padding: 20px 0; text-align: center;">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/tatsuki-chama.jpg" class="top-images no">
-    <div class="col-md-12 top-content" style="text-align: center;">
-      <h1>孫 ✕ 祖父母</h1>
-      <p>直想便は「孫 ✕ 祖父母」にフォーカスしたフォトグラファー派遣サービスです。<br>ただ撮るだけではなく、想い、想われ、想い合う時間を提供します。</p>
-      <div class="center">
-        <a href="?page_id=4" class="btn btn-default margin-top">ご利用方法</a>
-      </div>
+    <div class="col-md-5">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/motti-chama.jpg" style="width: 100%;">
     </div>
-  </div>
-  <div class="row" style="background-color: white; padding: 20px 0; text-align: center;">
-    <div class="col-md-12 top-content" style="text-align: center;">
-      <h1>撮る。</h1>
-      <p>直想便は、厳格な審査をもとに採用したフォトグラファーのみを派遣します。自分では撮れない素敵な写真を残しましょう！</p>
-    </div>
-    <div class="center">
-      <a href="#" class="btn btn-default">フォトグラファー</a>
-    </div>
-  </div>
-  <div class="row" style="background-color: #f6f7f9; padding: 20px 0; text-align: center;">
-    <img src="<?php echo get_template_directory_uri(); ?>/images/motti-chama.jpg" class="top-images no">
-    <div class="col-md-12 top-content" style="text-align: center;">
-      <h1>聴く。</h1>
-      <p>あなたは自分のおじいちゃんやおばあちゃんの好きな言葉を知っていますか？<br>直想便のフォトグラファーの役目はただ写真を撮るだけではありません。<br>むしろあなたとあなたのおじいちゃんやおばあちゃんの間に入り、普段は聴くことのない「想い」のやり取りを促すことにこそ彼らの役目があります。</p>
-    </div>
-    <div class="center">
-        <a href="?page_id=4" class="btn btn-default margin-top">フォトグラファー日記</a>
+    <div class="col-md-4" style="padding: 20px;">
+      <h4>孫 <span class="yellow">✕</span> 祖父母</h4>
+      <p>直想便は孫と祖父母にフォーカスしたフォトグラフ撮影サービスです。</p>
+      <h4>人に歴史あり。</h4>
+      <p style="padding-bottom: 20px;">私達が大切にしている言葉のひとつに<br>「人に歴史あり。」<br>というものがあります。<br>どんな人にでもそれぞれの人生の歴史がある。</p>
+      <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
     </div>
   </div>
 </div>
 
-<!-- 新着ニュースの部分 -->
+<!-- スライダー -->
+<div id="second-slider">
+  <h1>スライダー挿入予定</h1>
+</div>
 
-<div class="row">
-  <div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10 top-content">
-    <article>
-      <section>
-        <h2 class="section-title">お知らせ</h2>
-        <ul class="news-list">
-  <?php query_posts("cat=3 & showposts=3"); ?>
-        <?php
-        if(have_posts()):
-          while(have_posts()):
-            the_post();
-        ?>
-          <li>
-            <span class="date"><?php the_time('Y.n.j'); ?></span>
-            <span class="label-info"><?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->cat_name; } ?></span>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-          </li>
-          <?php
-            endwhile;
-          endif;
-          ?>
-        </ul>
-        <div class="center">
-          <a href="#" class="btn btn-default">過去のお知らせ</a>
+<div  style="background-color: white; padding: 30px 0;">
+  <div class="container">
+    <div class="row" style="margin: 40px 0;">
+      <div class="col-md-3" style="margin: 30px 0;">
+        <h2 class="roman"><span class="yellow">L</span>atest<br><span class="yellow">P</span>ost</h2>
+      </div>
+      <div class="col-md-5">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/motti-chama.jpg" style="width: 100%;">
+      </div>
+      <div class="col-md-4" style="padding-left: 30px;">
+        <p style="font-size: 14px;">孫 ✕ 祖母 / 東京 / 直想便</p>
+        <a class="yellow" style="font-size: 17px;">自立した愛される大人になってほしい。</a>
+        <p class="roman">Wednesday 21 June, 2017</p>
+        <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-offset-1 col-md-10">
+        <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/tatsuki-chama.jpg" style="width: 100%;">
+          <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+          <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+          <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+          <div style="margin-top: 10px;">
+            <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+          </div>
         </div>
-      </section>
-    </article>
+        <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/imaginekawabe.jpg" style="width: 100%;">
+          <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+          <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+          <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+          <div style="margin-top: 10px;">
+            <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/our-shadow.jpg" style="width: 100%;">
+          <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+          <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+          <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+          <div style="margin-top: 10px;">
+            <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/post-red.jpg" style="width: 100%;">
+          <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+          <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+          <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+          <div style="margin-top: 10px;">
+            <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+          </div>
+        </div>
+          <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" style="width: 100%;">
+            <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+            <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+            <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+            <div style="margin-top: 10px;">
+              <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/tatsuki-chama.jpg" style="width: 100%;">
+            <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+            <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+            <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+            <div style="margin-top: 10px;">
+              <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/lovepost-top.jpg" style="width: 100%;">
+            <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+            <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+            <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+            <div style="margin-top: 10px;">
+              <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/kameyama.jpg" style="width: 100%;">
+            <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+            <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+            <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+            <div style="margin-top: 10px;">
+              <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</div>
+<div id="chokuso-catalog">
+  <div class="container">
+    <div class="row" style="padding-top: 90px;">
+      <div class="col-md-12">
+        <h2 class="roman" style="font-weight: bold; color: white;">CHOKUSOPOST Catalogue</h2>
+        <a href="" title="" class="roman" style="color:white; border: 1px solid white; padding: 4px;">View the Catalogue</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container" id="get-social">
+  <div class="row">
+    <div class="col-md-3" style="padding-left: 30px;">
+      <h2 class="roman"><span class="yellow">G</span>et<br><span class="yellow">S</span>ocial</h2>
+      <p class="roman yellow" style="font-size: 20px;">→ <a href="" title="" class="roman">Instagram</a></p>
+      <p class="roman" style="font-size: 20px;">→ <a href="" title="" class="roman">Facebook</a></p>
+      <p class="roman" style="font-size: 20px;">→ <a href="" title=""" class="roman">Twitter</a></p>
+    </div>
+    <div class="col-md-9" style="padding: 10px;">
+      <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/tatsuki-chama.jpg" style="width: 100%;">
+        <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+        <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+        <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+        <div style="margin-top: 10px;">
+          <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/lovepost-top.jpg" style="width: 100%;">
+        <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+        <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+        <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+        <div style="margin-top: 10px;">
+          <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6 col-xs-6" style="padding: 8px;">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/kameyama.jpg" style="width: 100%;">
+        <p style="font-size: 12px; padding: 5px 0 0 5px;">祖父母 / 東京 / 直想便</p>
+        <a class="yellow" style="font-size: 15px;">テキストテキストテキストテキスト</a>
+        <p style="padding-top: 20px;">何は今はなはだその戦争児としてものの所へ取次いなけれあり。どうか十一月が解剖院は実にそのごろごろたありでもへ思わて来でにも反抗するないたて、ああにはありだなかっますます。</p>
+        <div style="margin-top: 10px;">
+          <a href="" class="roman" style="border: 1px black solid; padding: 4px; font-size: 15px;">more</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div style="background-color: #dab145; height: 300px;">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <h2 class="roman" style="color: white;">Application</h2>
+      </div>
+      <div class="col-md-6" style="text-align: center;">
+        <h1 style="border: 3px solid white; padding: 5px; margin-top: 130px;"><a href="" title="" style="color: white;">撮影お申込みはこちら！</a></h1>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<!-- 新着ニュースの部分 -->
+<div class="container" style="padding: 30px;">
+  <div class="row">
+    <div class="col-md-offset-2 col-md-8 col-sm-offset-1 col-sm-10 top-content" style="text-align: center;">
+      <article>
+        <section>
+          <h2 class="section-title">運営からのお知らせ</h2>
+          <ul class="news-list">
+    <?php query_posts("cat=3 & showposts=3"); ?>
+          <?php
+          if(have_posts()):
+            while(have_posts()):
+              the_post();
+          ?>
+            <li>
+              <span class="date roman"><?php the_time('Y.n.j'); ?></span>
+              <span class="label-info"><?php $cat = get_the_category(); $cat = $cat[0]; { echo $cat->cat_name; } ?></span>
+              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </li>
+            <?php
+              endwhile;
+            endif;
+            ?>
+          </ul>
+          <div class="center">
+            <a href="#" class="btn btn-default">過去のお知らせ</a>
+          </div>
+        </section>
+      </article>
+    </div>
   </div>
 </div>
 
