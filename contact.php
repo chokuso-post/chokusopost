437fd3fede868fe8f121ff9bd8a59607cebe24d7
磯上樹 <tatsuki5820iso@gmail.com>
2017-07-01 20:43:13 +0900

    contact page initial commit

--- contact.php
+++ contact.php
@@ -1,4 +1,3 @@
-
 <?php
 /*
 Template Name: contact
@@ -7,7 +6,20 @@ Template Name: contact
 
 <?php get_header(); ?>
 
-hogehgoeogyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+<div class="container">
+  <div class="row">
+    <h1 style="margin-top: 50px" class="col-xs-offset-1">Contact</h1>
+    <div class="col-lg-offset-3 col-xs-offset-1">
+      <h2>お申込みはこちら</h2>
+      <h2 class="yellow">CEO 河邉今人</h2>
+      <div class="col-xs-offset-3">
+        <p>メールアドレス：<<br>
+        電話番号：
+        </p>
+      </div>
 
+    </div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
