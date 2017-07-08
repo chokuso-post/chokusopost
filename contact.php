62aaff931d4b18063ad5b4e3ea8f0f94bb0a5a46
磯上樹 <tatsuki5820iso@gmail.com>
2017-07-09 01:05:06 +0900

    お問い合わせページ作りはじめ

--- contact.php
+++ contact.php
@@ -6,8 +6,23 @@ Template Name: contact
 ?>
 
 <?php get_header(); ?>
+<br>
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/contact.css">
 
-hogehgoeogyaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+<div class="container" style="margin-top: 120px;">
+  <div class="row">
+    <h1 class="yellow" style="font-size: 40px">Contact</h1>
+    <div class="contact-contents col-xs-offset-1">
+      <p class="contact-contents-header">直想便Email</p>
+      <p class="" style="font-size: 30px">chokusopost@gmail.com</p>
+    </div>
+    <div class="contact-contents col-xs-offset-1">
+      <h2 class="contact-contents-header">直想便代表　河邉今人</h2>
+    </div>
+  </div>
+</div>
 
 
 <?php get_footer(); ?>
+
+
