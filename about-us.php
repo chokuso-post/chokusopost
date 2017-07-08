fd5ee331ffdc5f91dae58b608d274f1305bb5179
磯上樹 <tatsuki5820iso@gmail.com>
2017-07-09 03:36:15 +0900

    運営ページ写真サイズ修正 段揃え中

--- about-us.php
+++ about-us.php
@@ -5,6 +5,7 @@ Template Name: about-us
 ?>
 
 <?php get_header(); ?>
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/about-us.css">
 
 <div class="container" style="background-color: #ffefff; width: 100%;">
   <div class="row" style="margin-top: 50px;">
@@ -16,28 +17,31 @@ Template Name: about-us
     <div class="col-md-offset-2 col-md-8 col-sm-12" style="padding: 10px;">
       <h1><span class="special-font">Chokusopost Members</span><br>メンバー</h1>
       <div class="member col-md-3 col-sm-10 col-sm-offset-1" style="padding: 10px;">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" alt="imajin" class="profile-picture">
-        <h3>河邉 今人</h3>
+        <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" alt="imajin" class="profile-picture" style="max-width: 100%;">
+        <h3 class="member-name"><span class="alphabet">CEO</span><br>河邉 今人</h3>
+        <p>いまじんです。</p>
+      </div>
+      <div class="member col-md-3 col-xs-10 col-xs-offset-1" style="padding: 10px;">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/isogami.jpg" alt="tatsuki" class="profile-picture">
+        <h3 class="member-name">CTO<br>磯上 樹</h3>
+        <p>直想便最高技術責任者のタツキです。使いやすいサービスを作れるよう頑張ります！</p>
+      </div>
+      <div class="member col-md-3 col-sm-10 col-sm-offset-1" style="padding: 10px;">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" alt="motthi" class="profile-picture">
+        <h3 class="member-name">Sales Manager <br>寺本 菜花</h3>
+        <p>もってぃです。</p>
+      </div>
+      <div class="member col-md-3 col-sm-10 col-sm-offset-1" style="padding: 10px;">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" alt="okasan" class="profile-picture">
+        <h3 class="member-name">GM <br>岡田 俊樹</h3>
+      </div>
+      <div class="member col-md-3 col-sm-10 col-sm-offset-1" style="padding: 10px;">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" alt="onusan" class="profile-picture">
+        <h3 class="member-name">CMO <br>小貫 将太</h3>
       </div>
       <div class="member col-md-3 col-sm-10 col-sm-offset-1" style="padding: 10px;">
         <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" alt="imajin" class="profile-picture">
-        <h3>磯上 樹</h3>
-      </div>
-      <div class="member col-md-3 col-sm-10 col-sm-offset-1" style="padding: 10px;">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" alt="imajin" class="profile-picture">
-        <h3>寺本 菜花</h3>
-      </div>
-      <div class="member col-md-3 col-sm-10 col-sm-offset-1" style="padding: 10px;">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" alt="imajin" class="profile-picture">
-        <h3>岡田 俊樹</h3>
-      </div>
-      <div class="member col-md-3 col-sm-10 col-sm-offset-1" style="padding: 10px;">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" alt="imajin" class="profile-picture">
-        <h3>小貫 将太</h3>
-      </div>
-      <div class="member col-md-3 col-sm-10 col-sm-offset-1" style="padding: 10px;">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/imajin.jpg" alt="imajin" class="profile-picture">
-        <h3>Sayuri Murooka</h3>
+        <h3 class="member-name">CFO <br>Sayuri Murooka</h3>
       </div>
     </div>
   </div>
